74fafc51ac3b4b5d56f762614a14449b54fd9b62
ManithMam <100299120+ManithMam@users.noreply.github.com>
2022-11-21 10:59:29 +0100

    Add basic weapon class architecture

---
+++ WASD/Assets/WeaponClass.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class WeaponClass : MonoBehaviour
+{
+    protected abstract string Name { get; }
+    protected abstract float Dmg { get; }
+    protected abstract float CritDmg { get; }
+    protected abstract float CritChance { get; }
+    protected abstract float Lifesteal { get; }
+    protected abstract float AtkSpeed { get; }
+    protected abstract int UpgradeLevel { get; }
+    protected abstract rarity RarityType { get; }
+
+    public enum rarity
+    {
+        common,
+        uncommon,
+        rare,
+        epic,
+        legendary
+    }
+    
+}
