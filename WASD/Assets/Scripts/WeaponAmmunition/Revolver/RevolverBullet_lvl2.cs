6593921d05da56e01e1f8f7d1f20186891394b86
ManithMam <100299120+ManithMam@users.noreply.github.com>
2023-01-10 20:49:49 +0100

    Implement Revolver_lvl1

---
+++ WASD/Assets/Scripts/WeaponAmmunition/Revolver/RevolverBullet_lvl2.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RevolverBullet_lvl2 : MonoBehaviour
+{
+    public float fadeOutTime = 0.25f;
+    private float bulletDamage;
+    private Revolver_lvl2 revolver;
+
+    private void Start()
+    {
+        revolver = GameObject.Find("Weapon").GetComponent<Revolver_lvl2>();
+        bulletDamage = revolver.Dmg;
+        ignorePhysicsOfPlayerAndAttacks();
+    }
+
+    private void ignorePhysicsOfPlayerAndAttacks()
+    {
+        Physics2D.IgnoreLayerCollision(6, 7);
+        Physics2D.IgnoreLayerCollision(7, 7);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            collision.gameObject.GetComponent<EnemyHealthScript>().DamageEnemy(bulletDamage);
+            Destroy(gameObject);
+        }
+
+        if (collision.gameObject.tag == "Object")
+        {
+            Destroy(gameObject);
+        }
+
+    }
+}
---
+++ WASD/Assets/Scripts/Weapons/Revolver/Revolver_lvl2.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Revolver_lvl2 : Weapon, IRangedWeapon
+{
+    protected override string Name => "Revolver";
+    public override float Dmg => 5;
+    protected override float CritDmg => 0.5f;
+    protected override float CritChance => 0.15f;
+    protected override float Lifesteal => 0;
+    protected override float AtkSpeed => 0.2f;
+    protected override int UpgradeLevel => 1;
+    protected override rarity RarityType => rarity.common;
+    public bool BulletIsTravelthrough { get; } = false;
+    public float timer { get; set; }
+    public float cooldown { get; set; } = 3f;
+    public GameObject bulletPrefab { get; set; }
+    public Transform firePoint { get; set; }
+    public float fireForce => 50f;
+
+    private void Start()
+    {
+        bulletPrefab = Resources.Load("Bullets/RevolverBullets/RevolverBullet") as GameObject;
+        firePoint = GameObject.Find("firePoint").transform;
+    }
+
+    public void FireBullet()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        bullet.GetComponent<Rigidbody2D>().AddForce(firePoint.right * fireForce, ForceMode2D.Impulse);
+    }
+
+    public override void attack()
+    {
+        timer += Time.deltaTime;
+        if (timer > cooldown)
+        {
+            FireBullet();
+            timer = 0;
+        }
+    }
+}
