18eb457b075b53486802c215356abd13ae74a1a2
ManithMam <100299120+ManithMam@users.noreply.github.com>
2023-01-18 09:04:52 +0100

    Implement GorillaArms_lvl2 and GorillaArms_lvl3
    Size of the sprite and push force now increases with each level.

---
+++ WASD/Assets/Scripts/Weapons/GorillaArms/GorillaArms_lvl2.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static Weapon;
+
+public class GorillaArms_lvl2 : Weapon, IMeleeWeapon
+{
+    public override string Name => "GorillaArms";
+    public override float Dmg => 0;
+    protected override float CritDmg => 0.5f;
+    protected override float CritChance => 0.15f;
+    protected override float Lifesteal => 0;
+    protected override float AtkSpeed => 0.2f;
+    protected override rarity RarityType => rarity.common;
+    public override int WeaponLevel => 1;
+    public GameObject attackPrefab { get; set; }
+    public Transform firePoint { get; set; }
+
+    public float timer { get; set; }
+    public float cooldown { get; set; } = 2f;
+
+    private float lifeTime = 0.25f;
+
+
+    private void Start()
+    {
+        attackPrefab = Resources.Load("MeleeAttacks/GorillaArm/GorillaArm_lvl2") as GameObject;
+        firePoint = GameObject.Find("firePointMelee").transform;
+    }
+
+    public void swingWeapon()
+    {
+        GameObject gorillaArm = Instantiate(attackPrefab, firePoint.position, firePoint.rotation);
+        Destroy(gorillaArm, lifeTime);
+    }
+
+    public override void attack()
+    {
+        timer += Time.deltaTime;
+        if (timer > cooldown)
+        {
+            swingWeapon();
+            timer = 0;
+        }
+    }
+}
