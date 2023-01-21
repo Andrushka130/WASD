89970ca01339edf2d2d4f9c3ee936b1c7c9bdbdb
kimberlykoehnke <96300055+kimberlykoehnke@users.noreply.github.com>
2023-01-21 09:38:39 +0100

    #64
    finished MainMenu, Kevin have to test leaderboard and that the highscore is showing

--- WASD/Assets/Scripts/UI/Leaderboard/EntryController.cs
+++ WASD/Assets/Scripts/UI/Leaderboard/EntryController.cs
@@ -1,25 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class EntryController : MonoBehaviour
 {
-    public EntryUI entryUI;
-    public PlayerData[] highscores;
+    [SerializeField] private Transform entryTemplate;
+    [SerializeField] private Transform entryContainer;
+  
    
-    void Start()
+    void Awake()
     {
-        /* 
+        
+        entryTemplate.gameObject.SetActive(false);
+
         StartCoroutine(Database.GetHighscore( result => {
-          highscores = PlayerData.ParseAll(result).Item;
+            PlayerData[] highscores = PlayerData.ParseAll(result).Items;
+                for(int i = 0; i < highscores.Length; i++)
+                {
+                Transform entry = Instantiate(entryTemplate, entryContainer);
+                Debug.Log("Instantiate");
+                entry.Find("TextPos").GetComponent<TextMeshProUGUI>().text = (i+1).ToString();
+                entry.Find("TextName").GetComponent<TextMeshProUGUI>().text = highscores[i].PlayerTag;
+                entry.Find("TextScore").GetComponent<TextMeshProUGUI>().text  = highscores[i].Highscore.ToString();
+                entry.gameObject.SetActive(true);       
+                }
         })); 
-        */
+ 
+      
+
+       
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
--- WASD/Assets/Scripts/UI/MainMenu.cs
+++ WASD/Assets/Scripts/UI/MainMenu.cs
@@ -2,12 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class MainMenu : MonoBehaviour
 {
-    [SerializeField]
-    private GameObject leaderboard;
-    private bool leaderboardIsNotActive = true;
+    [SerializeField] private GameObject leaderboard;
+    [SerializeField] private bool leaderboardIsNotActive;
+    [SerializeField] private TextMeshProUGUI highscoreText;
+
+     void Awake()
+    {
+        PlayerData _playerData = PlayerData.Instance;
+        if(_playerData.LoggedIn)
+        {
+           CheckHighscore(_playerData);
+        }
+        
+        highscoreText.text = _playerData.Highscore.ToString();
+        
+    }
+
+
 
     public void StartGame()
     {
@@ -30,5 +45,23 @@ public class MainMenu : MonoBehaviour
     {
         Debug.Log("Exit Game");
         Application.Quit();
+    }
+
+    private void CheckHighscore(PlayerData _playerData)
+    {
+        StartCoroutine(Database.GetHighscore(_playerData.PlayerTag, result => {
+
+            if(_playerData.Highscore < PlayerData.Parse(result).Highscore)
+            {
+                _playerData.Highscore = PlayerData.Parse(result).Highscore;
+                _playerData.SaveHighscore();
+            }
+            else
+            {
+                StartCoroutine(Database.UpdateHighscore(_playerData, result => {
+                    Debug.Log(result);
+                }));
+            }
+        }));
     }
 }
