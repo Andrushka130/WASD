98bf0aad7cea4fd8d0b58ace213100fe912609c2
Cipric Kevin <kc028@hdm-stuttgart.de>
2023-01-25 20:43:19 +0100

    implemented attribute icons

--- WASD/Assets/Scripts/Enums/AttributeResources.cs
+++ WASD/Assets/Scripts/Enums/AttributeResources.cs
@@ -2,15 +2,15 @@ using System;
 
 namespace AttributeResources
 {
-    public static class AttributePath
+    public static class AttributeIcons
     {
-        public static readonly string HealthPath = "Attribute/Health_Sprite.png";
-        public static readonly string AttackPath = "Attribute/Damage_Sprite.png";
-        public static readonly string CritChancePath = "Attribute/Crit_Chance_Sprite.png";
-        public static readonly string CritDamagePath = "Attribute/Crit_Damage_Sprite.png";
-        public static readonly string AttackSpeedPath = "Attribute/Attack_Speed_Sprite.png";
-        public static readonly string LuckPath = "Attribute/Luck_Sprite.png";
-        public static readonly string MovementSpeedPath = "Attribute/Speed_Sprite.png";
-        public static readonly string PsychoLevelPath = "Attribute/Psycho_Level_Sprite.png";
+        public static readonly string HealthIcon = "Attribute/Health_Sprite.png";
+        public static readonly string AttackIcon = "Attribute/Damage_Sprite.png";
+        public static readonly string CritChanceIcon = "Attribute/Crit_Chance_Sprite.png";
+        public static readonly string CritDamageIcon = "Attribute/Crit_Damage_Sprite.png";
+        public static readonly string AttackSpeedIcon = "Attribute/Attack_Speed_Sprite.png";
+        public static readonly string LuckIcon = "Attribute/Luck_Sprite.png";
+        public static readonly string MovementSpeedIcon = "Attribute/Speed_Sprite.png";
+        public static readonly string PsychoLevelIcon = "Attribute/Psycho_Level_Sprite.png";
     }
 }
