465113838e0e33a1f2dbf014e19d29e598d58de7
Andrushka130 <andreschwabauer1@gmail.com>
2023-02-14 20:23:00 +0100

    Fixed Bug for Build

--- WASD/Assets/Scripts/Enemy/EnemyAnimationHandler.cs
+++ WASD/Assets/Scripts/Enemy/EnemyAnimationHandler.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using static UnityEditor.PlayerSettings;
 using static UnityEngine.RuleTile.TilingRuleOutput;
 
 public class EnemyAnimationHandler : MonoBehaviour
